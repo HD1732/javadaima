41983f012d713ec0b8f8d81a7e2bf39e43a00066
yun_ye <1732398347@qq.com>
2022-02-15 22:58:33 +0800

    Initial commit

--- untitled/src/hello/helloworld.java
+++ untitled/src/hello/helloworld.java
@@ -4,7 +4,6 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println(sum(1,2));
-
     }
     public static int sum(int a,int b){
         return a+b;
